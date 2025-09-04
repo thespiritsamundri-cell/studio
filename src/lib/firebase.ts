da910ef909efacca78933bcc134386e356c9e04f
Cric Glee <cricglee@gmail.com>
2025-09-04 03:12:48 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAtnV9kiSJ-NFLfI6pG4LDvvLcjpRh_

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,13 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// TODO: PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE
 const firebaseConfig = {
-  "projectId": "educentral-mxfgr",
-  "appId": "1:93439797301:web:c0cd1d46e7588e4df4297c",
-  "storageBucket": "educentral-mxfgr.firebasestorage.app",
-  "apiKey": "AIzaSyAtnV9kiSJ-NFLfI6pG4LDvvLcjpRh_jtM",
-  "authDomain": "educentral-mxfgr.firebaseapp.com",
-  "messagingSenderId": "93439797301"
+  apiKey: "AIzaSyAtnV9kiSJ-NFLfI6pG4LDvvLcjpRh_jtM",
+  authDomain: "educentral-mxfgr.firebaseapp.com",
+  projectId: "educentral-mxfgr",
+  storageBucket: "educentral-mxfgr.firebasestorage.app",
+  messagingSenderId: "93439797301",
+  appId: "1:93439797301:web:c0cd1d46e7588e4df4297c"
 };
 
 // Initialize Firebase
