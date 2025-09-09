9a8c824999f5b615a0905026755c684018a65040
Cric Glee <cricglee@gmail.com>
2025-09-09 18:18:09 +0000

    again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,6 @@ import { fontVariables } from '@/app/font-config'
 import { ThemeProvider } from '@/components/layout/theme-provider'
 
 export const metadata = {
-  title: 'EduCentral',
   description: 'A comprehensive school management system.',
 }
 
