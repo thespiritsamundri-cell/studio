159fae45d3ccec52d5c7f916197f2772c5afd906
Cric Glee <cricglee@gmail.com>
2025-09-13 08:53:39 +0000

    Resolove

--- src/context/settings-context.tsx
+++ src/context/settings-context.tsx
@@ -25,7 +25,6 @@ export interface SchoolSettings {
   whatsappPhoneNumberId?: string;
   whatsappAccessToken?: string;
   
-  whatsappActive?: boolean;
   whatsappProvider?: 'ultramsg' | 'official' | 'none';
   whatsappConnectionStatus: 'untested' | 'connected' | 'failed';
   messageDelay: number;
@@ -58,11 +57,10 @@ export const defaultSettings: SchoolSettings = {
   schoolLogo: 'https://firebasestorage.googleapis.com/v0/b/educentral-mxfgr.appspot.com/o/settings%2FschoolLogo%2Flogo.png?alt=media&token=e1f06b6f-7f6a-4565-b17a-8b9a07106517',
   favicon: 'https://firebasestorage.googleapis.com/v0/b/educentral-mxfgr.appspot.com/o/settings%2Ffavicon%2Ffavicon.ico?alt=media&token=48c8b4a2-996a-4523-832f-410a3c26b527',
   principalSignature: 'https://firebasestorage.googleapis.com/v0/b/educentral-mxfgr.appspot.com/o/settings%2FprincipalSignature%2Fsignature.png?alt=media&token=8a5c3789-9a74-4b53-8b5e-404391694f8d',
-  whatsappApiUrl: 'https://api.ultramsg.com/instance141491',
+  whatsappApiUrl: 'https://api.ultramsg.com',
   whatsappApiKey: '4e8f26fx3a2yi942',
   whatsappInstanceId: 'instance141491',
   whatsappPriority: '10',
-  whatsappActive: true,
   whatsappProvider: 'ultramsg',
   whatsappConnectionStatus: 'untested',
   messageDelay: 2,
