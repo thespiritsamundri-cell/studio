847dae19e8be3d4035413cbbce91dafc1f6349d2
Cric Glee <cricglee@gmail.com>
2025-09-17 10:31:08 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type {Config} from 'tailwindcss';
 import plugin from 'tailwindcss/plugin';
 
 export default {
-  darkMode: ['class'],
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
