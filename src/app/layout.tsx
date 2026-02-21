76323b63b189c52059b90a4070cb3113c7f711f2
Cric Glee <cricglee@gmail.com>
2026-02-21 04:49:20 +0000

    I have generated new logos for my PWA application and I want to replace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="https://i.postimg.cc/zbjqz3Zv/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://i.postimg.cc/R6RvSq5q/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://i.postimg.cc/mzwTLhfg/favicon-16x16.png" />
         <meta name="theme-color" content="#4f46e5" />
       </head>
       <body suppressHydrationWarning>
